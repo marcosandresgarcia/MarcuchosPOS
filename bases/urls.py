adc59ed7f7334ababf7d521eebb064d02cef9296
Marcos Garcia <marcos.garcia@omnilatam.com>
2023-08-26 14:01:05 -0500

    add login template

---
+++ bases/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from django.contrib.auth import views as aut_views
+from bases.views import Home
+
+urlpatterns = [
+    path('', Home.as_view(), name='home'),
+
+    path('login/', aut_views.LoginView.as_view(template_name='bases/login.html'), name='login')
+]
---
+++ bases/views.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import generic
+
+
+# Create your views here.
+
+class Home(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'home/home.html'
+    login_url = '/admin'
